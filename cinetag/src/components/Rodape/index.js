37fc184400b778e7b34bf70af09ad88419aa88b2
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-03-28 11:44:21 -0300

    Rodape

---
+++ cinetag/src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css"
+
+function Rodape (){
+    return(
+        <footer className={styles.rodape}>
+            <span>Desenvolvido por Wesley</span>
+        </footer>
+    )
+}
+
+export default Rodape
--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,9 +1,11 @@
 import Cabecalho from "components/Cabecalho"
+import Rodape from "components/Rodape"
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Rodape />
         </>
     )
 }
