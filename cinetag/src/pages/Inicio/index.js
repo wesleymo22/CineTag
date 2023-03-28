d055d4842aa749e09e1d69e01445579ca97f48b4
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-03-28 15:57:06 -0300

    Lista dinamica

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho"
 import Card from "components/Card"
 import Rodape from "components/Rodape"
 import Titulo from "components/Titulo"
+import videos from "json/db.json"
+import styles from "./Inicio.module.css"
 
 function Inicio() {
     return (
@@ -12,7 +14,11 @@ function Inicio() {
             <Titulo>
                 <h1>Um lugar para guardar seus videos e filmes!</h1>
             </Titulo>
-            <Card id='1' titulo="Teste" capa="https://github.com/wesleymo22.png"/>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
