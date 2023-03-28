43d2423b4a18966138df9d5d903ec1b259c1ef30
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-03-28 15:13:43 -0300

    Banner e Titulo

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,10 +1,16 @@
+import Banner from "components/Banner"
 import Cabecalho from "components/Cabecalho"
 import Rodape from "components/Rodape"
+import Titulo from "components/Titulo"
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus videos e filmes!</h1>
+            </Titulo>
             <Rodape />
         </>
     )
