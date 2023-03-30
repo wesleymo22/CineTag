9660ad597d5d56d3477144ae9f0501a1918aef0d
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-03-29 21:25:53 -0300

    Criando contextos

--- cinetag/src/pages/Favoritos/index.js
+++ cinetag/src/pages/Favoritos/index.js
@@ -1,9 +1,15 @@
-
+import Banner from "components/Banner"
+import Titulo from "components/Titulo"
+import styles from "./Favoritos.module.css"
 
 function Favoritos () {
     return(
         <>
+            <Banner imagem="favoritos" />
+            <Titulo><h1>Meus Favoritos</h1></Titulo>
+            <section className={styles.container}>
 
+            </section>
         </>
     )
 }
