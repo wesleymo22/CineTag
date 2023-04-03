c1f3fda5101d818aee3ffd9eb00e0bfae3e52486
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-04-03 15:38:06 -0300

    Alterar icone favorito

--- cinetag/src/components/Card/index.js
+++ cinetag/src/components/Card/index.js
@@ -1,15 +1,25 @@
+import { useFavoritoContext } from "context/Favoritos"
 import styles from "./Card.module.css"
 import iconeFavoritar from "./favoritar.png"
+import iconeDesFavoritar from "./desfavoritar.png"
+
 
 function Card ({ id, titulo, capa }) {
+    const {favorito, adicionarFavorito} = useFavoritoContext()
+    const eFavorito = favorito.some((fav) => fav.id === id);
+    const icone = eFavorito ? iconeDesFavoritar : iconeFavoritar;
+
     return(
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
             <img 
-            src={iconeFavoritar}
+            src={icone}
              alt="Favoritar"
              className={styles.favoritar}
+             onClick={() => {
+                adicionarFavorito({id, titulo, capa});
+             }}
             />
         </div>
     )
