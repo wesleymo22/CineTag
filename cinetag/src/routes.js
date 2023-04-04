6c85af1c33d218b7466ec0e6227b28b637ce5865
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-04-04 10:05:22 -0300

    Rotas alinhadas

--- cinetag/src/routes.js
+++ cinetag/src/routes.js
@@ -1,28 +1,21 @@
-import Cabecalho from "components/Cabecalho"
-import Container from "components/Container"
-import Rodape from "components/Rodape"
-import FavoritosProvider from "context/Favoritos"
 import Favoritos from "pages/Favoritos"
 import Inicio from "pages/Inicio"
 import NaoEncontrada from "pages/NaoEncontrada"
+import PaginaBase from "pages/PaginaBase"
 import Player from "pages/Player"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container >
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />} />
-                        <Route path="/favoritos" element={<Favoritos />} />
-                        <Route path="/:id" element={<Player />} />
-                        <Route path="*" element={<NaoEncontrada />} />
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />} />
+                    <Route path="favoritos" element={<Favoritos />} />
+                    <Route path=":id" element={<Player />} />
+                    <Route path="*" element={<NaoEncontrada />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
