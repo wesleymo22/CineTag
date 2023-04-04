a205b8af87c2564c0550ae22f6a1b74b26e0104e
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-04-04 10:53:55 -0300

    Consumindo API useEffect e fecth com parametros

--- cinetag/src/pages/Player/index.js
+++ cinetag/src/pages/Player/index.js
@@ -1,20 +1,26 @@
 import Banner from "components/Banner"
 import Titulo from "components/Titulo"
 import { useParams } from "react-router-dom"
-import videos from 'json/db.json'
 import styles from "./Player.module.css"
 import NaoEncontrada from "pages/NaoEncontrada"
+import { useEffect, useState } from "react"
 
 function Player() {
+    const [video, setVideo] = useState()
     const parametros = useParams();
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id)
-    })
 
-    if(!video){
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/wesleymo22/cinetag-api/videos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, [])
+
+    if (!video) {
         return <NaoEncontrada />
     }
-    
+
     return (
         <>
             <Banner imagem="player" />
@@ -22,13 +28,13 @@ function Player() {
                 <h1>Player</h1>
             </Titulo>
             <section className={styles.container}>
-                <iframe 
-                width="100%" 
-                height="100%" 
-                src={video.link}
-                title={video.titulo}
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe
+                    width="100%"
+                    height="100%"
+                    src={video.link}
+                    title={video.titulo}
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </section>
         </>
     )
